cdc4749f04ec70321b60bd6f8aec6d50b6b456d1
dreamblader <gm3_gab@hotmail.com>
2019-07-09 15:03:22 -0300

    slow start (learning structure)

---
+++ sample/character.py
@@ -0,0 +1,13 @@
+import game
+
+class Character:
+    def __init__(self,name,level,game):
+        self.name = name
+        self.level = level
+        self.game = game
+
+    @classmethod
+    def funcname(parameter_list):
+        #stuff here
+        return null
+        
---
+++ sample/game.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class Game(Enum):
+    DS1 = 1
+    DS2 = 2
+    DS3 = 3
