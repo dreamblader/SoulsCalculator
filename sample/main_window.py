5d1075c9fb890a12c63623cb12d3c41ba496dcf4
dreamblader <gm3_gab@hotmail.com>
2019-09-04 14:01:28 -0300

    learning how to work with packages (setup.py)

--- sample/main_window.py
+++
@@ -1,9 +0,0 @@
-# MAIN CALCULATOR WINDOW!
-from tkinter import *
-
-window = Tk()
-window.title("SoulsCalculator")
-Label(window, text='TEST').pack()
-window.mainloop()
-
-#MAIN IDEA: TABS >>> GUI CLASS WITH MENU + CONTENT (menu change content)
---
+++ src/soulscalc/main_window.py
@@ -0,0 +1,10 @@
+# MAIN CALCULATOR WINDOW!
+from tkinter import *
+
+def main():
+    window = Tk()
+    window.title("SoulsCalculator")
+    Label(window, text='TEST').pack()
+    window.mainloop()
+
+#MAIN IDEA: TABS >>> GUI CLASS WITH MENU + CONTENT (menu change content)
