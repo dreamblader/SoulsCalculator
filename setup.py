143b138a66a7a7943ca739169437e9829cf00a9a
dreamblader <gm3_gab@hotmail.com>
2019-09-04 14:14:06 -0300

    setup.py changes

--- setup.py
+++ setup.py
@@ -17,32 +17,6 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
 
-    # There are some peculiarities on how to include package data for source
-    # distributions using setuptools. You also need to add entries for package
-    # data to MANIFEST.in.
-    # See https://stackoverflow.com/questions/7522250/
-    #include_package_data=True,
-
-    # This is a trick to avoid duplicating dependencies between both setup.py and
-    # requirements.txt.
-    # requirements.txt must be included in MANIFEST.in for this to work.
-    # It does not work for all types of dependencies (e.g. VCS dependencies).
-    # For VCS dependencies, use pip >= 19 and the PEP 508 syntax.
-    #   Example: 'requests @ git+https://github.com/requests/requests.git@branch_or_tag'
-    #   See: https://github.com/pypa/pip/issues/6162
-    #install_requires=open('requirements.txt').readlines(),
-    #zip_safe=False,
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-
     entry_points={
         'console_scripts': ['souls=soulscalc.main_window:main'],
     }
